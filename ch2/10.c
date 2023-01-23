00a183eab80e08b1d1cc6d28cd033308cc2ad3ed
Imre Nagi <imre.nagi@gojek.com>
2023-01-23 00:38:37 -0500

    Rewrite the function lower, which converts upper case letters to lower case, with a conditional expression instead of if-else.
    Signed-off-by: Imre Nagi <imre.nagi@gojek.com>

---
+++ ch2/10.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/*
+Exercise 2-10. Rewrite the function lower, which converts upper case letters to lower case, with a conditional expression instead of if-else.
+*/
+
+/* lower: convert c to lower case; ASCII only */
+int lower(int c) {
+  return (c >= 'A' && c <= 'Z') ? c + 'a' - 'A' : c;  
+}
+
+int main() {
+  printf("%c %c\n", 'C', lower('C'));
+  printf("%c", 'c');
+  return 0;
+}
